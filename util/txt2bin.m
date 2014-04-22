dbb4e94db065d00142bf28ee08be0cac75a72cae
htellez <htellez@nearbpo.com>
2014-04-22 11:28:14 -0500

    finishing txt2bin function.

--- util/txt2bin.m
+++ util/txt2bin.m
@@ -1,4 +1,8 @@
 function bin = txt2bin(txt)
 	bin = dec2bin(toascii(txt));
 	bin = reshape(bin, 1, size(bin, 1)*size(bin, 2));
+	
+	while(mod(size(bin, 2), 4)  != 0)
+		bin = ["0", bin];
+	endwhile
 endfunction

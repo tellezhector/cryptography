0e6aa3218e301630d92bc71fbb9ace60a72c5eaf
htellez <htellez@nearbpo.com>
2014-04-22 11:31:50 -0500

    finishing hex2txt function.

--- util/hex2txt.m
+++ util/hex2txt.m
@@ -1,5 +1,10 @@
 function text = hex2txt(hex)
 	if(mod(size(hex, 2), 2) != 0)
 		hex = ["0", hex];
-	endfunction
+	endif
+	s = size(hex, 2)/2;
+	
+	i = 2*(1:s)-1;
+	pairs = [hex(:, i)', hex(:, i+1)'];
+	text = char(hex2dec(pairs))';	
 endfunction

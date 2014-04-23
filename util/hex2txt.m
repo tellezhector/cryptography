b34b568169581d0083205b3299a0d9ca2a8b33bd
htellez <htellez@nearbpo.com>
2014-04-23 04:06:27 -0500

    Updatinb functio to convert hex to txt.

--- util/hex2txt.m
+++ util/hex2txt.m
@@ -1,10 +1,4 @@
 function text = hex2txt(hex)
-	if(mod(size(hex, 2), 2) != 0)
-		hex = ["0", hex];
-	endif
-	s = size(hex, 2)/2;
-	
-	i = 2*(1:s)-1;
-	pairs = [hex(:, i)', hex(:, i+1)'];
+	pairs = split_hex_in_pairs(hex);
 	text = char(hex2dec(pairs))';	
 endfunction
